75b8c731add20b1f5ea26dbd49bf3f4b4489b3b1
osmanfil <68877154+osmanfil@users.noreply.github.com>
2026-05-23 02:07:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -659,7 +659,7 @@ function setTheme(themeClass, themeName, element) {
 
 
 // ==================== API KEY YÖNETİM MOTORU ====================
-let GEMINI_API_KEY = "AIzaSyBziQ7zPXmcdVdIkECmXtZOgPszIGrZbG8"; // Not: Eğer anahtarın devamı varsa tırnağı kapatmadan önce ekle.
+let GEMINI_API_KEY = ""; // Not: Eğer anahtarın devamı varsa tırnağı kapatmadan önce ekle.
 
 window.addEventListener('DOMContentLoaded', () => {
     updateApiUIState();
