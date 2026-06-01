3a19386a937709f2b224e44e4fe645eddf2152a7
osmanfil <68877154+osmanfil@users.noreply.github.com>
2026-06-02 01:40:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -651,7 +651,7 @@ html_kodu = """
                             • Google AI Studio entegrasyonu tamamlandı.<br>
                             • Kullanıcı dostu hızlı API Key alma arayüzü eklendi.<br>
                             • Gereksiz asistan nezaketleri kaldırıldı. Lich artık seninle doğrudan ve kararlı bir şekilde iletişime geçiyor.
-                            • Not: Lich'in bilinci, Osman Fil tarafından sürekli güncellenen ve geliştirilen bir yapıdır.
+                            • Lich'in bilinci, Osman Fil tarafından sürekli güncellenen ve geliştirilen bir yapıdır.
                         </div>
                     </div>
 
